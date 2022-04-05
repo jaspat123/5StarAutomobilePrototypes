6a9c3e4f33983c0838c30dae35fc2b1e608c632e
jaspat123 <102557744+jaspat123@users.noreply.github.com>
2022-04-04 21:52:23 -0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace CustomerInformation
+{
+    public partial class Form1 : Form
+    {
+
+        SqlCommand cmd;
+        SqlConnection con;
+        SqlDataAdapter da;
+        int results;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+            if (txtCusName.Text == "" || txtEmail.Text == "" || txtAddSt.Text == "" || txtCity.Text == "" || txtProv.Text == "" || txtPostCode.Text == "" || txtPhone.Text == "")
+            {
+                MessageBox.Show("Please enter missing fields");
+                return;
+            }
+
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\temp\Jasmine\CustomerEntry\CustomerInformation\Data.mdf;Integrated Security=True");
+            con.Open();
+            cmd = new SqlCommand("INSERT INTO Customerdb (CustomerName,EmailAddress,AddressStreet,City,Province,PostalCode,PhoneNumber) VALUES (@CustomerName,@EmailAddress,@AddressStreet,@City,@Province,@PostalCode,@PhoneNumber)", con);
+            cmd.Parameters.AddWithValue("@CustomerName", txtCusName.Text);
+            cmd.Parameters.AddWithValue("@EmailAddress", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@AddressStreet", txtAddSt.Text);
+            cmd.Parameters.AddWithValue("@City", txtCity.Text);
+            cmd.Parameters.AddWithValue("@Province", txtProv.Text);
+            cmd.Parameters.AddWithValue("@PostalCode", txtPostCode.Text);
+            cmd.Parameters.AddWithValue("@PhoneNumber", txtPhone.Text);
+            results = cmd.ExecuteNonQuery();
+            MessageBox.Show("Your data has been inputed.");
+           
+       }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ dbo.Customerdb.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customerdb] (
+    [CustomerName]  VARCHAR (50) NOT NULL,
+    [EmailAddress]  VARCHAR (50) NOT NULL,
+    [AddressStreet] VARCHAR (50) NOT NULL,
+    [City]          VARCHAR (50) NOT NULL,
+    [Province]      VARCHAR (50) NOT NULL,
+    [PostalCode]    CHAR (10)    NOT NULL,
+    [PhoneNumber] INT NOT NULL, 
+    CONSTRAINT [PK_Customerdb] PRIMARY KEY ([CustomerName])
+);
+
